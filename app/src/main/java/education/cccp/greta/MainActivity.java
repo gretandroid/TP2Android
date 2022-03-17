68e3370c8f4750ac8532d6f0ca3d190a8a3b0012
cheroliv <40507040+cheroliv@users.noreply.github.com>
2022-03-17 11:57:45 +0100

    ajout d'id sur les composants UI, création d'un event listener sur le bouton send a partir des wizards de l'IDE.

--- app/src/main/java/education/cccp/greta/MainActivity.java
+++ app/src/main/java/education/cccp/greta/MainActivity.java
@@ -1,14 +1,33 @@
 package education.cccp.greta;
 
-import androidx.appcompat.app.AppCompatActivity;
+import static android.widget.Toast.LENGTH_LONG;
+import static android.widget.Toast.makeText;
+import static education.cccp.greta.R.layout.activity_main;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    EditText lastNameEditText;
+    EditText firstNameEditText;
+    Button sendButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(activity_main);
+        lastNameEditText = findViewById(R.id.lastNameEditText);
+        firstNameEditText = findViewById(R.id.firstNameEditText);
+    }
+
+    public void sendButtonOnClick(View view) {
+        makeText(this,
+                lastNameEditText.getText().toString(),
+                LENGTH_LONG)
+                .show();
     }
 }
